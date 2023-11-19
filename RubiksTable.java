b4b31a88cdfe447aa5571a58c0289c2dd9b07d1a
Dancs Kornél <113311203+dancs-kornel@users.noreply.github.com>
2023-11-19 18:53:18 +0100

    Update RubiksTable.java

--- RubiksTable.java
+++ RubiksTable.java
@@ -3,7 +3,7 @@ import controller.gameController;
 /**
  * The main class to initiate and start the game.
  */
-public class Main {
+public class RubiksTable {
     public static void main(String[] args) {
         gameController gameController = new gameController(3);
     }
